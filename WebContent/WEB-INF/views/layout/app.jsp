ae4ebce20b5b47c038573447c064e44db13f7f8f
mayukorin <19t7b2fx@gmail.com>
2020-08-14 22:53:52 +0900

    Add company new

---
+++ WebContent/WEB-INF/views/layout/app.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>新聞管理システム</title>
+        <link rel="stylesheet" href="<c:url value='/css/reset.css' />">
+        <link rel="stylesheet" href="<c:url value='/css/style.css' />">
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <div id="header_menu">
+                    <h1><a href="<c:url value='/'/>">新聞管理システム</a></h1>
+                </div>
+                <c:if test="${sessionScope.login_company != null}">
+                    <div id="company_name">
+                        <c:out value="${sessionScope.login_company.name}"/>&nbsp;さん&nbsp;&nbsp;&nbsp;
+                        <a href="<c:url value='/logout' />">ログアウト</a>
+                    </div>
+                </c:if>
+            </div>
+            <div id="content">
+                ${param.content}
+            </div>
+            <div id="footer">
+                by Mayuko Inoue
+            </div>
+        </div>
+    </body>
+</html>
---
+++ src/controllers/company/CompaniesNewServlet.java
@@ -0,0 +1,42 @@
+package controllers.company;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.company;
+
+/**
+ * Servlet implementation class CompaniesNewServlet
+ */
+@WebServlet("/companies/new")
+public class CompaniesNewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CompaniesNewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("company", new company());
+
+        RequestDispatcher rd =request.getRequestDispatcher("/WEB-INF/views/companies/new.jsp");
+        rd.forward(request, response);
+
+    }
+
+}
