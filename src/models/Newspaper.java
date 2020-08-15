71067b25c74bca7a5ca81a5b7d1bc89395389832
mayukorin <19t7b2fx@gmail.com>
2020-08-15 22:50:02 +0900

    Add Newspaper

---
+++ src/models/Newspaper.java
@@ -0,0 +1,53 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="newspaper")
+public class Newspaper {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="year")
+    private Integer year;
+
+    @Column(name="month")
+    private Integer month;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+
+
+
+
+}
